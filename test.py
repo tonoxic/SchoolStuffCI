a401aa8dc4bc1df88dc4a7bb57a3bbca9124b47e
tonoxic <47722391+tonoxic@users.noreply.github.com>
2019-10-29 15:28:24 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import calculator
+
+class Test(unittest.TestCase):
+  def test_add(self):
+    self.assertEqual(calculator.add(2,3),5)
+    
+  def test_sub(self):
+    self.assertEqual(calculator.sub(2,3),-1)
+    
+  def test_mult(self):
+    self.assertEqual(calculator.mult(2,3),6)
+  
+  def test_div(self):
+    self.assertEqual(calculator.div(6,3),2)
+ 
+ if __name__=='__main__':
+    unittest.main()
+    
+    
