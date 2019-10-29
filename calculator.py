5b3f5f21bf82678f62b1fdcb0e8a77cdf9c79cb6
tonoxic <47722391+tonoxic@users.noreply.github.com>
2019-10-29 14:36:34 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+def add(x, y):
+    return x + y 
+
+def sub(x, y):
+    return x - y 
+
+def mult(x, y):
+    return x + y 
+
+def div(x, y):
+    return x / y
+
+def cry(x,y):
+    return x*x*y
